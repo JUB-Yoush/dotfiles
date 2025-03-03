35b32599f6b3099237fc085e875646ea0c689a6b
JUB_Yoush <yoush1337@gmail.com>
2025-03-02 19:33:16 -0500

    treesit was changed somehow

--- .config/nvim/lua/plugins/gd-treesit.lua
+++ .config/nvim/lua/plugins/gd-treesit.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     ensure_installed = { "gdscript", "godot_resource", "gdshader" },
+    indent = { enable = true },
     highlight = {
       enable = true,
     },
