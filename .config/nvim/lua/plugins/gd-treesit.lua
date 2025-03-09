eace6983a8a2c51bef29129c87f08dfd57e06e7e
JUB_Yoush <yoush1337@gmail.com>
2025-03-09 19:08:47 -0400

    treesit

---
+++ .config/nvim/lua/plugins/gd-treesit.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    ensure_installed = { "gdscript", "godot_resource", "gdshader" },
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+  },
+}
